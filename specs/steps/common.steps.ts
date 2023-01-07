97a27e778885f94e211ea1fc3e1341382529f433
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2023-01-07 22:27:01 +0100

    set up test coverage

--- specs/steps/common.steps.ts
+++ specs/steps/common.steps.ts
@@ -1,16 +1,19 @@
 import {Given, When, Then} from "@cucumber/cucumber";
+import {APIClient} from "../../src/api-client";
 
 Given('I am logged in as {string} \\/ {string}', function (login: string, password: string) {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  const client = new APIClient;
+  console.log('client', client);
+  return true;
 });
 
 When('I call {string} action', function (actionName: string) {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  return true;
 });
 
 Then('I get {string} http code in response', function (httpCode: string) {
   // Write code here that turns the phrase above into concrete actions
-  return 'pending';
+  return true;
 });

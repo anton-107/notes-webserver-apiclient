e87b944a8b5b05e065336f4f0a51e947faa4edc1
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2023-01-07 22:16:17 +0100

    set up cucumber tests

---
+++ specs/steps/common.steps.ts
@@ -0,0 +1,16 @@
+import {Given, When, Then} from "@cucumber/cucumber";
+
+Given('I am logged in as {string} \\/ {string}', function (login: string, password: string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+When('I call {string} action', function (actionName: string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('I get {string} http code in response', function (httpCode: string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});

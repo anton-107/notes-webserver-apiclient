24d654d7c129f327f5d7eef939b244302f47bdca
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2023-01-07 22:28:46 +0100

    set up prettier formatting

--- specs/steps/common.steps.ts
+++ specs/steps/common.steps.ts
@@ -1,19 +1,22 @@
-import {Given, When, Then} from "@cucumber/cucumber";
-import {APIClient} from "../../src/api-client";
+import { Given, When, Then } from "@cucumber/cucumber";
+import { APIClient } from "../../src/api-client";
 
-Given('I am logged in as {string} \\/ {string}', function (login: string, password: string) {
+Given(
+  "I am logged in as {string} \\/ {string}",
+  function (login: string, password: string) {
+    // Write code here that turns the phrase above into concrete actions
+    const client = new APIClient();
+    console.log("client", client);
+    return true;
+  }
+);
+
+When("I call {string} action", function (actionName: string) {
   // Write code here that turns the phrase above into concrete actions
-  const client = new APIClient;
-  console.log('client', client);
   return true;
 });
 
-When('I call {string} action', function (actionName: string) {
-  // Write code here that turns the phrase above into concrete actions
-  return true;
-});
-
-Then('I get {string} http code in response', function (httpCode: string) {
+Then("I get {string} http code in response", function (httpCode: string) {
   // Write code here that turns the phrase above into concrete actions
   return true;
 });
--- src/api-client.ts
+++ src/api-client.ts
@@ -1,3 +1,1 @@
-export class APIClient {
-  
-}
+export class APIClient {}

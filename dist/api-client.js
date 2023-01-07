8b877135f3f2647cae673f6d44c7bb73783c2807
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2023-01-07 22:17:49 +0100

    setup build

---
+++ dist/api-client.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.APIClient = void 0;
+class APIClient {
+}
+exports.APIClient = APIClient;
+//# sourceMappingURL=api-client.js.map

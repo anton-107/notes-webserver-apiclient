63b3716f76fc16f0767f11be49d4c893cbe204e3
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2023-01-10 22:32:44 +0100

    read authentication token from sign response cookie

--- src/api-client.ts
+++ src/api-client.ts
@@ -1,3 +1,4 @@
+import cookie from "cookie";
 import fetch from "node-fetch";
 
 interface Notebook {
@@ -10,18 +11,50 @@ export interface HttpResponse<T> {
 }
 
 export class APIClient {
+  private authenticationToken: string | undefined = undefined;
+
   public async authorize(login: string, password: string): Promise<boolean> {
-    const response = await fetch(`api-endpoint-address`, {});
-    console.log(
-      "Trying to authorize: ",
-      login,
-      "using password of length",
-      password.length,
-      response
-    );
-    return false;
+    const response = await fetch(`${process.env.API_ROOT}/signin`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        "user-login": login,
+        "user-password": password,
+      }),
+    });
+    const responseHeaders = response.headers;
+    const statusCode = response.status;
+
+    if (statusCode !== 200) {
+      return false;
+    }
+
+    const cookies = cookie.parse(responseHeaders.get("set-cookie"));
+    if (!cookies["Authentication"]) {
+      return false;
+    }
+
+    this.authenticationToken = cookies["Authentication"];
+    return true;
   }
   public async createNotebook(): Promise<HttpResponse<Notebook>> {
+    // const response = await fetch(`${process.env.API_ROOT}/notebook`, {
+    //   method: "post",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     // "Cookie": cookie.serialize('Authentication', this.authenticationToken),
+    //   },
+    //   body: JSON.stringify({
+    //     "notebook-name": "This notebook is created programmatically",
+    //   }),
+    // });
+    // const responseHeaders = response.headers;
+    // const statusCode = response.status;
+
+    // console.log("createNotebook resp", statusCode, responseHeaders);
+
     return {
       httpCode: 0,
       body: undefined,
